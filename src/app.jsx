8295269e6e6ec639b0bf71d4172b9af316c69927
Navpreet Singh <navpreet.singh.k@gmail.com>
2016-02-03 00:19:43 +0530

    updated the config file

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');  //React Router to define routes
-var HashHistory = require('react-router/lib/hashhistory'); //maintain logs or history of page where route is currently on
+var HashHistory = require('react-router/lib/hashHistory'); //maintain logs or history of page where route is currently on
 var Router = ReactRouter.Router;   //Router deciding which content to show on page
 var Route = ReactRouter.Route;   //Object to configure the route
+
 
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
+    	<h1> Working Fine </h1>
       {this.props.children}
     </h1>
   }
@@ -28,11 +30,12 @@ var Child2 = React.createClass({
 });
 
 var routes = (
-	<Router history={new HashHistory}>
+	< Router history={HashHistory}>
 		<Route path="/" component={Hello}>
 			<Route path="1" component={Child1} />
 			<Route path="2" component={Child2} />
 		</Route>
 	</Router> 
+)
 
 ReactDOM.render(routes, document.querySelector('.container'));
