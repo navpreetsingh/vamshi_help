ce2b54d46f77f37782b9bcba7115cac1810ceb32
Navpreet Singh <navpreet.singh.k@gmail.com>
2016-02-02 22:56:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# vamshi_help
+
+Run The below commands
+=> git clone 
+=> cd vamshi_help
+=> sudo npm install\
+=> npm install --save react-router
+=> npm install -g gulp
+=> gulp
+
+
+Change your code in file 'src/app.jsx'
+2 components are there child1 and child2
+
---
+++ src/app.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');  //React Router to define routes
+var HashHistory = require('react-router/lib/hashhistory'); //maintain logs or history of page where route is currently on
+var Router = ReactRouter.Router;   //Router deciding which content to show on page
+var Route = ReactRouter.Route;   //Object to configure the route
+
+var Hello = React.createClass({
+  render: function() {
+    return <h1 className="red">
+      {this.props.children}
+    </h1>
+  }
+});
+
+//  GET   localhost:8000/#/1
+var Child1 = React.createClass({
+	render: function() {
+		return <h1> I m the child1 </h1>		
+	}
+});
+
+//  GET  localhost:8000/#/2
+var Child2 = React.createClass({
+	render: function() {
+		return <h1> I m the child2 </h1>		
+	}
+});
+
+var routes = (
+	<Router history={new HashHistory}>
+		<Route path="/" component={Hello}>
+			<Route path="1" component={Child1} />
+			<Route path="2" component={Child2} />
+		</Route>
+	</Router> 
+
+ReactDOM.render(routes, document.querySelector('.container'));
